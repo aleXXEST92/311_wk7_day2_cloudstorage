b683baeeadb5dd4b25085d87a9102a5c61597386
Alex Munoz <anmunoz24@gmail.com>
2019-12-11 18:35:31 -0800

    service accounts

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const fs = require("fs");
 const {Storage} = require('@google-cloud/storage');
 
 const storage = new Storage({
-  projectId: 'aca-311',
-  keyFilename: './aca-311-d864d06c299f.json'
+  projectId: 'my-first-project-258101',
+  keyFilename: './My First Project-18bb828b4e75.json'
 })
 
 
-storage.bucket("aca-311-files").upload("./bruce.jpg", {
+storage.bucket("alex_aca_my_harddrive").upload("./bruce.jpg", {
     gzip: true,
     metadata: {
         cacheControl: 'public, max-age=31536000',
